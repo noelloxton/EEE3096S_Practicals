1a8b1a3582ac872a182aed286b3275e4fc91b70b
noelloxton <noeljloxton@gmail.com>
2019-07-27 15:51:00 +0200

    took away unused code and started prep for counter

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -11,10 +11,16 @@ Date: 4/8/2019
 import RPi.GPIO as GPIO  #import Raspberry Pi GPIO python module
 from time import sleep  #import the sleep function from the time module
 
-#event detection method
-def button_callback(channel):
-	print("button was pushed!")
+#button1/black_button detection method
+def button1_increment(channel):
+	print("black button was pushed!\n")
+	print("incrementing...\n")
 	GPIO.output(10, GPIO.HIGH)
+
+def button2_decrement(channel):
+	print("red button was pushed!\n")
+	print("decrementing...\n")
+	GPIO.output(11, GPIO.HIGH)
 
 #set numbering system to BCM
 GPIO.setmode(GPIO.BOARD)
@@ -22,37 +28,16 @@ GPIO.setmode(GPIO.BOARD)
 #set up input channel with pull down control.
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)  #set pin 10 to be an output pin and set its initial value to low
+#set pin 10 to be an output pin and set its initial value to low
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
 
 #set up event detection on pin 7 for button push rising edge
 GPIO.add_event_detect(7, GPIO.RISING, callback=button_callback, bouncetime=300)
 
-
 # main method
 def main():
 
-	sleep(1)
-
-	#set up input channel without pull-up
-	#GPIO.setup(7, GPIO.IN)
-
-	#read input from gpio 7
-#	input_value = GPIO.input(7)
-
-	#set up GPIO output channel on pin 8 with initial low value
-#	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-
-
-#    	GPIO.setmode(GPIO.BOARD) # Use Physical pin numbering
-
-#    	while True: #run forever
-#    		if GPIO.input(7) == GPIO.HIGH:
-#			print("Button was pushed!")
-#			GPIO.output(10, GPIO.HIGH) #Turn on
-#		sleep(1)
-#		GPIO.output(8, GPIO.LOW)  #Turn off
-#		sleep(1)
-
+	sleep(1)  #process to wait for button push
 
 # Only run the functions if 
 if __name__ == "__main__":
