7fe64505973a69fe59a4f84fca90e4901e1cfe94
noelloxton <noeljloxton@gmail.com>
2019-07-28 21:52:35 +0200

    made the buttons and leds work with interrupts

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -10,29 +10,51 @@ Date: 4/8/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO  #import Raspberry Pi GPIO python module
 from time import sleep  #import the sleep function from the time module
+import itertools
+
+#make array variable storing itertools.product
+#global i[] = itertools.product([0,1],repeat=3)
+#global int x = 0
 
 #button1/black_button detection method
 def button1_increment(channel):
-	print("black button was pushed!\n")
+	print("black button was pushed!")
 	print("incrementing...\n")
 	GPIO.output(10, GPIO.HIGH)
-
+	GPIO.output(11, GPIO.LOW)
+#button2/red_button detection method
 def button2_decrement(channel):
-	print("red button was pushed!\n")
+	print("red button was pushed!")
 	print("decrementing...\n")
+	GPIO.output(10, GPIO.LOW)
 	GPIO.output(11, GPIO.HIGH)
 
 #set numbering system to BCM
 GPIO.setmode(GPIO.BOARD)
 
-#set up input channel with pull down control.
+#ignore warning for now
+GPIO.setwarnings(False)
+
+#set up pin 7 to be an input channel with pull down control.
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#set up pin 8 to be an input channel with pull down control
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #set pin 10 to be an output pin and set its initial value to low
 GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
 
+#set pin 11 to be an output pin and set its initial value to low
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+
+#set pin 12 to be an output pin and set its initial value to low
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+
 #set up event detection on pin 7 for button push rising edge
-GPIO.add_event_detect(7, GPIO.RISING, callback=button_callback, bouncetime=300)
+GPIO.add_event_detect(7, GPIO.RISING, callback=button1_increment, bouncetime=300)
+
+#set up event detection on pin 8 for button push rising edge
+GPIO.add_event_detect(8, GPIO.RISING, callback=button2_decrement, bouncetime=300)
 
 # main method
 def main():
