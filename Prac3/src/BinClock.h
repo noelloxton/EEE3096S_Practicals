bbd9b1a97c5d12ab6e8b95a8931c8938e6dcfe3a
noelloxton <noeljloxton@gmail.com>
2019-08-17 15:37:03 +0200

    modified pinouts

--- Prac3/src/BinClock.h
+++ Prac3/src/BinClock.h
@@ -24,9 +24,9 @@ const char HOUR = 0x02;
 const char TIMEZONE = 2; // +02H00 (RSA)
 
 // define pins
-const int LEDS[] = {0,2,3,25,7,22,21,27,4,6}; //H0-H4, M0-M5
+const int LEDS[] = {7,0,2,3,12,13,14,21,22,23}; //H0-H4, M0-M5
 const int SECS = 1;
-const int BTNS[] = {5,30}; // B0, B1
+const int BTNS[] = {15,16}; // B0, B1
 
 
 #endif
